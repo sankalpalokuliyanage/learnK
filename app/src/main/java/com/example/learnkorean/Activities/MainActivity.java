dd4fcfe0d8b95026f6e1da7294883ad8ad08f213
= <c.s.lokuliyanage@wlv.ac.uk>
2024-01-20 18:24:44 +0900

    fixed some errors

--- app/src/main/java/com/example/learnkorean/Activities/MainActivity.java
+++ app/src/main/java/com/example/learnkorean/Activities/MainActivity.java
@@ -1,38 +1,16 @@
 package com.example.learnkorean.Activities;
 
-import static androidx.navigation.Navigation.findNavController;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.ui.NavigationUI;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
 import com.example.learnkorean.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
