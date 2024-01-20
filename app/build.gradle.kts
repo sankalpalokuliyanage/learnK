7d638d46a74cf371066a426d9ac844afb62446d4
= <c.s.lokuliyanage@wlv.ac.uk>
2024-01-20 18:18:59 +0900

    removed unnecessary implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,32 +50,6 @@ dependencies {
     implementation("androidx.navigation:navigation-runtime:2.7.6")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
-    //rounded imageview
-    implementation ("com.makeramen:roundedimageview:2.3.0")
 
-    //Glide
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-
-    implementation ("com.intuit.sdp:sdp-android:1.0.6")
-            implementation ("com.intuit.ssp:ssp-android:1.0.6")
-
-    //recycler view
-            implementation ("androidx.recyclerview:recyclerview:1.3.0")
-
-    //material design
-    implementation ("com.google.android.material:material:1.8.0")
-
-    //multidex
-            implementation ("com.android.support:multidex:1.0.3")
-
-    //retrofit
-            implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-            implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
-
-    //swipe refresh layout
-            implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-            implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.25")
 
 }
