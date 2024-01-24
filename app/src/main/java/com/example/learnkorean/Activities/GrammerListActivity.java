6453f1bf0c838dbea3084e097ecdf0386694e820
= <c.s.lokuliyanage@wlv.ac.uk>
2024-01-24 18:21:42 +0900

    create activity file for list grammer lesson names

---
+++ app/src/main/java/com/example/learnkorean/Activities/GrammerListActivity.java
@@ -0,0 +1,16 @@
+package com.example.learnkorean.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learnkorean.R;
+
+public class GrammerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grammer_list);
+    }
+}
